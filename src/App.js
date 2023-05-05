6830987b392d7d6c36c98fabf8b0cf6b38c727cb
Anatoli Dimitrov <anatoli.dimitrov@kyndryl.com>
2023-05-05 12:23:54 +0300

    POST fetch test

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+import React, { useState } from 'react';
 
 import { AuthContext } from "./services/AuthContext";
 import { useLocalStorage } from "./hooks/useLocalStorage";
@@ -10,12 +11,34 @@ import { Login } from "./components/auth/Login";
 function App() {
     const [auth, setAuth] = useLocalStorage('b9189a47-1fd1-4367-8d47-a003149fbe58', {});
 
-    const userAuth = (data) => {
-        setAuth(data);
+    const [file, setFile] = useState(null);
+
+    const handleFileChange = (event) => {
+        setFile(event.target.files[0]);
     };
 
-    const userLogout = () => {
-        setAuth({});
+    const handleUpload = () => {
+        const formData = new FormData();
+        formData.append('file', file);
+
+        fetch('https://cpd-cp4ba.apps.ocp.lab.lan/bawaut/automationservices/rest/RBAWS/ReactJS%20API/TestResource', {
+            method: 'POST',
+            body: formData
+        })
+            .then(response => {
+                console.log('Upload successful!');
+            })
+            .catch(error => {
+                console.error('Error uploading file:', error);
+            });
+
+        const userAuth = (data) => {
+            setAuth(data);
+        };
+
+        const userLogout = () => {
+            setAuth({});
+        };
     };
 
     return (
@@ -27,6 +50,11 @@ function App() {
                     </Route>
                     <Route path="/authentication/login" element={<Login />} />
                 </Routes>
+            </div>
+
+            <div>
+                <input type="file" onChange={handleFileChange} />
+                <button onClick={handleUpload} disabled={!file}>Upload</button>
             </div>
         </AuthContext.Provider>
     );
